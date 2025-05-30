a2d2cbcac5ca8ff26de3b7c1a16b35fec43f3e43
imerfe <imerfe222@gmail.com>
2025-05-30 11:42:27 +0500

    Create home.py

---
+++ lesson-1/homework/home.py
@@ -0,0 +1,30 @@
+#Given a side of square. Find its perimeter and area.
+a=23
+p=4*(a)
+S=a**2
+print('perimetr:',p)
+print('area:',S)
+#Given diameter of circle. Find its length.
+d=30
+pi=3.14
+l=pi*d
+print('length_of_circle:',l)
+#Given two numbers a and b. Find their mean.
+a=87
+b=3
+o=(a+b)/2
+
+print('orta_arifmetik',o)
+#Given two numbers a and b. Find their sum, product and square of each number.
+
+a=2
+b=4
+
+sum=a+b
+x=a*b
+a2=a**2
+b2=b**2
+print('sum',sum)
+print('kopaytmasi',x)
+print('kvadrati a-niki',a2,
+      'kvadrati b-niki',b2)
